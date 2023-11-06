4befddcbdfeed0a747be9c592be5537ac850e9e9
puzmarta <marta00puz@gmail.com>
2023-11-06 15:03:27 +0100

    Adding SignInActivity, CreateAccountActivity and basic navigation

--- app/src/main/java/com/example/aplikacjaandroid/SignInActivity.kt
+++ app/src/main/java/com/example/aplikacjaandroid/SignInActivity.kt
@@ -1,12 +1,9 @@
 package com.example.aplikacjaandroid
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,6 +13,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -23,15 +21,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat.startActivity
 import com.example.aplikacjaandroid.ui.theme.AplikacjaAndroidTheme
 
-class MainActivity : ComponentActivity() {
+class SignInActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -41,7 +38,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    WelcomeView()
+                    SignInView()
+
                 }
             }
         }
@@ -49,37 +47,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun WelcomeView(){
-    Welcome(modifier = Modifier
+fun SignInView(){
+    SignIn(modifier = Modifier
         .fillMaxSize()
-        .wrapContentSize(Alignment.Center), LocalContext.current)
+        .wrapContentSize(Alignment.Center))
+}
+
+@Preview
+@Composable
+fun SignIn(modifier : Modifier = Modifier){
+
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally){
+        Text(text = stringResource(R.string.zaloguj))
+        Image( painter = painterResource(R.drawable.icon_avatar), contentDescription = "Avatar icon")
+        Spacer(modifier = Modifier.height(16.dp))
+        SimpleTextField(label = "Adres email")
+        SimpleTextField(label = "Hasło")
+        Button(onClick = { TODO() }) {
+            Text(stringResource(R.string.zaloguj))
+        }
+    }
 }
 
 @Composable
-fun Welcome(modifier : Modifier = Modifier, context: Context){
-
-
-
-    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally){
-        Image( painter = painterResource(R.drawable.piggy_icon), contentDescription = "Piggybank icon")
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = stringResource(R.string.rozpocznij))
-        Text(text = stringResource(R.string.zaloguj_lub_utworz_konto))
-        Button(onClick = {
-            val navigate = Intent(context, SignInActivity::class.java)
-            context.startActivity(navigate)
-
-        }) {
-            Text(stringResource(R.string.zaloguj))
-        }
-        Button(onClick = {
-            val navigate = Intent(context, CreateAccountActivity::class.java)
-            context.startActivity(navigate)
-
-
-        }) {
-            Text(stringResource(R.string.utworz_konto))
-        }
+fun SimpleTextField(modifier : Modifier = Modifier, label: String) {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    Column(modifier = modifier){
+        Text(text = label)
+        TextField(
+            value = text,
+            onValueChange = { newText ->
+                text = newText
+            }
+        )
     }
 
 }
+
