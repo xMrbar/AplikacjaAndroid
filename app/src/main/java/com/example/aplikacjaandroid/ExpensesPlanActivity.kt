83dba058377146f8bbeb5770857b5435fd3b19bc
Mr_bar_Laptop <b02@op.pl>
2023-11-07 09:55:08 +0100

    Added ExpensesPlanActivity

--- app/src/main/java/com/example/aplikacjaandroid/ExpensesPlanActivity.kt
+++ app/src/main/java/com/example/aplikacjaandroid/ExpensesPlanActivity.kt
@@ -1,16 +1,49 @@
 package com.example.aplikacjaandroid
 
+import android.app.Activity
+import android.app.LauncherActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.aplikacjaandroid.ui.theme.AplikacjaAndroidTheme
+import com.google.android.gms.wallet.button.ButtonConstants
 
 class ExpensesPlanActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +55,7 @@ class ExpensesPlanActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ExpensesPlanView()
                 }
             }
         }
@@ -30,17 +63,157 @@ class ExpensesPlanActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ExpensesPlanView() {
+    ExpensesPlan(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center), LocalContext.current)
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    AplikacjaAndroidTheme {
-        Greeting("Android")
+fun ExpensesPlan(modifier : Modifier = Modifier, context: Context) {
+    val localActivity = (LocalContext.current as? Activity)
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally){
+        Text(text = stringResource(R.string.PlanowanieBudzetu),
+            color = MaterialTheme.colorScheme.primary,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Row(){
+            Button(
+                modifier = Modifier
+                    .width(150.dp)
+                    .height(50.dp),
+                onClick = {
+
+                },
+                colors = ButtonDefaults.textButtonColors(MaterialTheme.colorScheme.background)
+            ) {
+                Text(
+                    text=stringResource(R.string.przychody),
+                    color=MaterialTheme.colorScheme.tertiary)
+            }
+            Spacer(modifier = Modifier.width(40.dp))
+            Button(
+                modifier = Modifier
+                    .width(150.dp)
+                    .height(50.dp)
+                    .border(
+                        2.dp,
+                        MaterialTheme.colorScheme.tertiary,
+                        shape = MaterialTheme.shapes.extraLarge),
+                onClick = {
+                    val intentButtonPBA = Intent(context, RevenuesPlanActivity::class.java)
+                    context.startActivity(intentButtonPBA)
+                    localActivity?.finish()
+                },
+                colors = ButtonDefaults.textButtonColors(MaterialTheme.colorScheme.tertiary)) {
+                Text(
+                    stringResource(R.string.wydatki),
+                    color=MaterialTheme.colorScheme.background)
+            }
+        }
+        Spacer(modifier = Modifier.width(5.dp))
+        Box(
+            modifier = Modifier
+                .background(color = MaterialTheme.colorScheme.primary, shape = CircleShape)
+                .size(210.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                modifier = modifier,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(id = R.string.planowaneDochodyWMiesiacu),
+                    color = MaterialTheme.colorScheme.background,
+                    fontSize = 20.sp,
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = stringResource(id = R.string.planowaneDochodyWMiesiacu_kwota),
+                    color = MaterialTheme.colorScheme.background,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(5.dp))
+        LazyColumn(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+        ) {
+            /*itemsIndexed(listItems) { index, listItem ->
+                ListItemView(listItem, index)
+            }*/
+            items(6) {
+                Test()
+            }
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(
+            modifier = Modifier
+                .width(350.dp)
+                .height(50.dp),
+            onClick = {
+                TODO()
+            },
+            colors = ButtonDefaults.textButtonColors(MaterialTheme.colorScheme.secondary)
+        ) {
+            Text(stringResource(R.string.dodaj),
+                color=MaterialTheme.colorScheme.background)
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(
+            modifier = Modifier
+                .width(350.dp)
+                .height(50.dp),
+            onClick = {
+                TODO()
+            },
+            colors = ButtonDefaults.textButtonColors(MaterialTheme.colorScheme.secondary)
+        ) {
+            Text(stringResource(R.string.usun),
+                color=MaterialTheme.colorScheme.background)
+        }
     }
 }
+
+
+/*@Composable
+fun ListItemView(listItem: LauncherActivity.ListItem, index: Int) {
+    val backgroundColor = if (index % 2 == 0) {
+        MaterialTheme.colorScheme.background
+    } else {
+        MaterialTheme.colorScheme.secondary
+    }
+
+    val textColor = if (index % 2 == 0) {
+        MaterialTheme.colorScheme.secondary
+    } else {
+        MaterialTheme.colorScheme.background
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .background(backgroundColor)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = listItem.imageResource),
+                contentDescription = null
+            )
+            // Tekst
+            Text(listItem.text, color = Color.White, modifier = Modifier.padding(start = 16.dp))
+        }
+    }
+}
+*/
