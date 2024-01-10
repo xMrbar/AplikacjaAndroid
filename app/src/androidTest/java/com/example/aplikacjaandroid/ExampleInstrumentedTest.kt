fb8ced8ac8a09c14a8559ba97825df7d3884eb78
Mr_bar_Laptop <b02@op.pl>
2024-01-10 17:58:28 +0100

    Added some tests
    Added first GUI test

--- app/src/androidTest/java/com/example/aplikacjaandroid/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/aplikacjaandroid/ExampleInstrumentedTest.kt
@@ -41,4 +41,70 @@ class ExampleInstrumentedTest {
             ItemData(3, 2, "Samolot", BigDecimal(1000), "10.12.2022")
         ), result)
     }
+
+    @Test
+    fun testWriteAppendAndRead() {
+        val fileName = "test_file.txt"
+        val items = listOf(
+            "1;32re;6.12.2023;258;INCOME",
+            "2;Samochód;3.3.2024;33.33;INCOME",
+            "3;Samolot;10.12.2022;1000;INCOME"
+        )
+        val fileManager = FileManager(fileName)
+        fileManager.writeStringItemsToFile(appContext, items)
+        fileManager.appendToFile("testAppend;7.12.2023;258.13;HOME", appContext)
+        val result = fileManager.readItemsFromFile(appContext)
+        assertEquals(listOf(
+            ItemData(1, 2, "32re", BigDecimal(258), "6.12.2023" ),
+            ItemData(2, 2, "Samochód", BigDecimal("33.33"), "3.3.2024"),
+            ItemData(3, 2, "Samolot", BigDecimal(1000), "10.12.2022"),
+            ItemData(4, 4, "testAppend", BigDecimal("258.13"), "7.12.2023")
+        ), result)
+    }
+
+    @Test
+    fun testDelete() {
+        val fileName = "test_file.txt"
+        val items = listOf(
+            "1;32re;6.12.2023;258;INCOME",
+            "2;Samochód;3.3.2024;33.33;INCOME",
+            "3;Samolot;10.12.2022;1000;INCOME"
+        )
+        val fileManager = FileManager(fileName)
+        fileManager.writeStringItemsToFile(appContext, items)
+        fileManager.deleteItemFromFile(appContext, 1)
+        val result = fileManager.readItemsFromFile(appContext)
+        assertEquals(listOf(
+            ItemData(1, 2, "32re", BigDecimal(258), "6.12.2023" ),
+            ItemData(3, 2, "Samolot", BigDecimal(1000), "10.12.2022")
+        ), result)
+    }
+
+    @Test
+    fun testCouner() {
+        val counter = Counter(appContext)
+        val fileName = "expenses.txt"
+        val fileManager = FileManager(fileName)
+        fileManager.writeStringItemsToFile(appContext, listOf(
+                "1;32re;6.1.2024;258;INCOME",
+                "2;Samochód;3.1.2024;33.33;INCOME",
+                "3;Samolot;10.1.2022;1000;INCOME",
+                "4;Dom;3.4.2024;300;INCOME"
+        ))
+        assertEquals(counter.countExpensesThisMonth(), BigDecimal("291.33"))
+    }
+
+    @Test
+    fun testCouner1() {
+        val counter = Counter(appContext)
+        val fileName = "expensesPlan.txt"
+        val fileManager = FileManager(fileName)
+        fileManager.writeStringItemsToFile(appContext, listOf(
+            "1;32re;CO MIESIĄC;258;INCOME",
+            "2;Samochód;CO 3 MIESIĄCE;33.33;INCOME",
+            "3;Samolot;CO MIESIĄC;1000;INCOME",
+            "4;Dom;CO 12 MIESIĄCY;300;INCOME"
+        ))
+        assertEquals(counter.countExpensesPlan(), BigDecimal("1591.33"))
+    }
 }
---
+++ app/src/androidTest/java/com/example/aplikacjaandroid/GUITest.kt
@@ -0,0 +1,248 @@
+package com.example.aplikacjaandroid
+
+
+import android.util.Log
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.aplikacjaandroid.ui.screens.AccountScreen
+import com.example.aplikacjaandroid.ui.screens.AddNewExpensePlanScreen
+import com.example.aplikacjaandroid.ui.screens.AddNewExpenseScreen
+import com.example.aplikacjaandroid.ui.screens.AddNewOutPlanScreen
+import com.example.aplikacjaandroid.ui.screens.AddNewRevenuePlanScreen
+import com.example.aplikacjaandroid.ui.screens.AddNewRevenueScreen
+import com.example.aplikacjaandroid.ui.screens.ChangePasswordScreen
+import com.example.aplikacjaandroid.ui.screens.CreateAccountScreen
+import com.example.aplikacjaandroid.ui.screens.CreateFile
+import com.example.aplikacjaandroid.ui.screens.ExpensesPlanScreen
+import com.example.aplikacjaandroid.ui.screens.ExpensesScreen
+import com.example.aplikacjaandroid.ui.screens.HistoryAnalysisScreen
+import com.example.aplikacjaandroid.ui.screens.MainMenuScreen
+import com.example.aplikacjaandroid.ui.screens.RevenuesPlanScreen
+import com.example.aplikacjaandroid.ui.screens.RevenuesScreen
+import com.example.aplikacjaandroid.ui.screens.SignInScreen
+import com.example.aplikacjaandroid.ui.screens.StatisticsScreen
+import com.example.aplikacjaandroid.ui.screens.UserAccountScreen
+import com.example.aplikacjaandroid.ui.screens.WelcomeScreen
+import com.google.firebase.auth.FirebaseAuth
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+class ControllerForTest{
+    @Composable
+    fun initNav(navController: NavHostController = rememberNavController(),
+                start: AppScreen){
+        NavHost(
+            navController = navController,
+            startDestination = start.name,
+            modifier = Modifier
+        ) {
+            composable( route = AppScreen.Welcome.name){
+                WelcomeScreen(modifier = Modifier.fillMaxSize(),
+                    onSignInButtonClickedHandler = {navController.navigate(AppScreen.SignIn.name)},
+                    onCreateAccountButtonClickedHandler = {navController.navigate(AppScreen.CreateAccount.name)}
+                )
+            }
+
+            composable( route = AppScreen.SignIn.name){
+                SignInScreen(modifier = Modifier.fillMaxSize(),
+                    onSignInButtonClickedHandler = {navController.navigate(AppScreen.MainMenu.name)}
+                )
+            }
+
+            composable( route = AppScreen.CreateAccount.name){
+                CreateAccountScreen(modifier = Modifier.fillMaxSize(),
+                    onCreateAccountButtonClickedHandler = {navController.navigate((AppScreen.MainMenu.name))}
+                )
+            }
+
+            composable( route = AppScreen.MainMenu.name){
+                CreateFile(LocalContext.current)
+                MainMenuScreen(modifier = Modifier.fillMaxSize(),
+                    onAccountBalanceButtonClickedHandler = {navController.navigate((AppScreen.AccountBalance.name))},
+                    onBudgetPlanButtonClickedHandler = {navController.navigate((AppScreen.RevenuesPlan.name))},
+                    onStatisticsButtonClickedHandler = {navController.navigate((AppScreen.Statistics.name))},
+                    onHistoryAnalysisButtonClickedHandler = {navController.navigate((AppScreen.HistoryAnalysis.name))},
+                    onUserAccountButtonClickedHandler = {navController.navigate((AppScreen.UserAccount.name))},
+                    onLogOutButtonClickedHandler = { signOut(navController) }
+                )
+            }
+
+            composable( route = AppScreen.Statistics.name){
+                StatisticsScreen(modifier = Modifier.fillMaxSize())
+            }
+
+            composable( route = AppScreen.UserAccount.name){
+                UserAccountScreen(
+                    modifier = Modifier.fillMaxSize(),
+                    onChangePasswordButtonClickedHandler = {navController.navigate((AppScreen.ChangePassword.name))},
+                    onDeleteAccountButtonClickedHandler =  {navigateToWelcome(navController)}
+                )
+            }
+
+            composable( route = AppScreen.ChangePassword.name){
+                ChangePasswordScreen(modifier = Modifier.fillMaxSize())
+            }
+
+            composable( route = AppScreen.AccountBalance.name){
+                AccountScreen(modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Alignment.Center),
+                    onRevenuesButtonClickedHandler = { navigateDirectTo(navController, AppScreen.Revenues) },
+                    onExpensesButtonClickedHandler = { navigateDirectTo(navController, AppScreen.Expenses) },
+                    onAddNewOutPlanButtonClickedHandler = { navController.navigate((AppScreen.AddNewOutPlan.name))}
+                )
+            }
+
+            composable( route = AppScreen.Revenues.name) {
+                RevenuesScreen(modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Alignment.Center),
+                    onAccountBalanceButtonClickedHandler = { navigateDirectTo(navController, AppScreen.AccountBalance) },
+                    onExpensesButtonClickedHandler = { navigateDirectTo(navController, AppScreen.Expenses)},
+                    onAddNewRevenueButtonClickedHandler = { navController.navigate((AppScreen.AddNewRevenue.name))}
+                )
+            }
+
+            composable( route = AppScreen.Expenses.name) {
+                ExpensesScreen(modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Alignment.Center),
+                    onAccountBalanceButtonClickedHandler = { navigateDirectTo(navController, AppScreen.AccountBalance)},
+                    onRevenuesButtonClickedHandler = { navigateDirectTo(navController, AppScreen.Revenues)},
+                    onAddNewExpenseButtonClickedHandler = { navController.navigate((AppScreen.AddNewExpense.name)) }
+                )
+            }
+
+            composable( route = AppScreen.RevenuesPlan.name){
+                RevenuesPlanScreen(modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Alignment.Center),
+                    onExpensesPlanButtonClickedHandler = { navigateDirectTo(navController, AppScreen.ExpensesPlan) },
+                    onAddRevenuesPlanAddButtonClieckedHandler = { navController.navigate((AppScreen.AddNewRevenuePlan.name)) })
+            }
+
+            composable( route = AppScreen.ExpensesPlan.name){
+                ExpensesPlanScreen(modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Alignment.Center),
+                    onRevenuesPlanButtonClickedHandler = { navigateDirectTo(navController, AppScreen.RevenuesPlan) },
+                    onAddExpensesPlanAddButtonClieckedHandler = { navController.navigate((AppScreen.AddNewExpensePlan.name)) })
+            }
+
+            composable( route = AppScreen.AddNewExpensePlan.name){
+                AddNewExpensePlanScreen(modifier = Modifier
+                    .fillMaxSize(),
+                    onRevenuesAddButtonClickedHandler = { navigateDirectTo(navController, AppScreen.AddNewRevenuePlan) },
+                    onClickGoBack = { navController.popBackStack()
+                        navigateDirectTo(navController, AppScreen.ExpensesPlan)})
+            }
+
+            composable( route = AppScreen.AddNewRevenuePlan.name){
+                AddNewRevenuePlanScreen(modifier = Modifier
+                    .fillMaxSize(),
+                    onExpenseAddButtonClickedHandler = { navigateDirectTo(navController, AppScreen.AddNewExpensePlan) },
+                    onClickGoBack = { navController.popBackStack()
+                        navigateDirectTo(navController, AppScreen.RevenuesPlan)})
+            }
+
+            composable( route = AppScreen.AddNewExpense.name){
+                AddNewExpenseScreen(modifier = Modifier
+                    .fillMaxSize(),
+                    onRevenuesAddButtonClickedHandler = { navigateDirectTo(navController, AppScreen.AddNewRevenue) },
+                    onOutPlanAddButtonClickedHandler = { navigateDirectTo(navController, AppScreen.AddNewOutPlan) },
+                    onClickGoBack = { navController.popBackStack()
+                        navigateDirectTo(navController, AppScreen.Expenses)})
+            }
+
+            composable( route = AppScreen.AddNewRevenue.name){
+                AddNewRevenueScreen(modifier = Modifier
+                    .fillMaxSize(),
+                    onExpensesAddButtonClickedHandler = { navigateDirectTo(navController, AppScreen.AddNewExpense)  },
+                    onOutPlanAddButtonClickedHandler = { navigateDirectTo(navController, AppScreen.AddNewOutPlan) },
+                    onClickGoBack = { navController.popBackStack()
+                        navigateDirectTo(navController, AppScreen.Revenues)})
+            }
+
+            composable( route = AppScreen.AddNewOutPlan.name){
+                AddNewOutPlanScreen(modifier = Modifier
+                    .fillMaxSize(),
+                    onRevenuesAddButtonClickedHandler = { navigateDirectTo(navController, AppScreen.AddNewRevenue) },
+                    onExpensesAddButtonClickedHandler = { navigateDirectTo(navController, AppScreen.AddNewExpense) },
+                    onClickGoBack = { navController.popBackStack()
+                        navigateDirectTo(navController, AppScreen.AccountBalance)})
+            }
+
+            //---------------------------------------------------------------------------
+            // Not decoupled yet, still activities
+
+            composable( route = AppScreen.HistoryAnalysis.name){
+                HistoryAnalysisScreen(modifier = Modifier.fillMaxSize())
+            }
+
+        }
+    }
+
+    // pops up BackStack until it reaches Welcome destination
+    private fun navigateToWelcome(navController: NavController){
+        navController.popBackStack(AppScreen.Welcome.name, inclusive = false)
+    }
+
+    // signs current user out and pops up BackStack until it reaches Welcome destination
+    private fun signOut(navController: NavController){
+        val auth: FirebaseAuth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+        auth.signOut()
+        if (user != null) {
+            Log.d("FirebaseAuth", "User ${user.email} signed out")
+            navController.popBackStack(AppScreen.Welcome.name, inclusive = false)
+        }
+        else{
+            Log.d("FirebaseAuth", "Unable to sign out current user")
+        }
+
+    }
+
+    // pops the stack and navigates to destination of given name
+    private fun navigateDirectTo(navController: NavController, appScreen: AppScreen){
+        navController.popBackStack()
+        navController.navigate(appScreen.name)
+    }
+}
+
+@RunWith(AndroidJUnit4::class)
+class GUITest {
+    private val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+    private val controllerForTest = ControllerForTest()
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    private fun setMenu() {
+        composeTestRule.setContent {
+            controllerForTest.initNav(start = AppScreen.MainMenu)
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Test
+    fun testButtonClick() {
+        setMenu()
+        composeTestRule.onNodeWithText(appContext.getString(R.string.button1Text)).performClick()
+        composeTestRule.onNodeWithText(appContext.getString(R.string.accountBalance)).assertExists()
+    }
+}
